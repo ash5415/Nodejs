ba42079bbbebeccf2ba8c1772f5568867679183f
ashok <ashok5415.v@gmail.com>
2023-07-05 16:09:26 +1000

    redis

---
+++ redis/app.js
@@ -0,0 +1,29 @@
+const express =require('express');
+
+const app= express();
+
+const port = 3000;
+
+const redis= require('redis');
+
+const client =redis.createClient({
+    port:6379,
+    host:'127.0.0.1'
+});
+
+client.set('userVisits',0);
+
+app.get('/getVisits',(req,resp)=>{
+    const userVisits=data;
+
+    const currentVisits=data+1;
+
+    client.set('userVisits',currentVisits);
+    resp.send(200).json({
+        result:'successfully getting the user counts!!!!!!'
+    });
+})
+
+app.listen(port,()=>{
+    console.log(`server up and running on port ${port}`);
+});
